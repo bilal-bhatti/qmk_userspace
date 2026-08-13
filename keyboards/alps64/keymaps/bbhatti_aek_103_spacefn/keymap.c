9a7b995560a1d9dd013f4b705e59ea47cb4b2da2
Bilal Bhatti <bbhatti@gmail.com>
2026-08-12 23:51:06 -0500

    alps64: use current mouse keycode names
    KC_BTN1/KC_BTN2/KC_MS_* are the deprecated aliases. Rename to
    MS_BTN1/MS_BTN2/MS_LEFT/MS_DOWN/MS_UP/MS_RGHT in both spacefn keymaps.
    No behaviour change.

--- keyboards/alps64/keymaps/bbhatti_aek_103_spacefn/keymap.c
+++ keyboards/alps64/keymaps/bbhatti_aek_103_spacefn/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,    KC_F1,     KC_F2,    KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,      KC_F12,    KC_DEL,
 		_______,   _______,   _______,  _______,   _______,   _______,   KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    _______,   _______,     _______,   _______,
 		_______,   _______,   _______,  _______,   _______,   _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   _______,   _______,     _______,
-		_______,   _______,   _______,  KC_BTN2,   KC_BTN1,   KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,   _______,   _______,   _______,
+		_______,   _______,   _______,  MS_BTN2,   MS_BTN1,   MS_LEFT,   MS_DOWN,   MS_UP,     MS_RGHT,   _______,   _______,   _______,
 		_______,   _______,   _______,     _______,           _______,   _______,   _______)
 };
 
--- keyboards/alps64/keymaps/bbhatti_hhkb_spacefn/keymap.c
+++ keyboards/alps64/keymaps/bbhatti_hhkb_spacefn/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,   KC_F11,    KC_F12,    KC_NO,   KC_DEL, 
 		KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_NO,    KC_NO,     KC_NO,     KC_NO,
 		KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT,  KC_NO,     KC_NO,
-		KC_NO,    KC_NO,   KC_NO,   KC_BTN2, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_NO,   KC_NO,    KC_NO,     KC_NO,
+		KC_NO,    KC_NO,   KC_NO,   MS_BTN2, MS_BTN1, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT,  KC_NO,   KC_NO,    KC_NO,     KC_NO,
 		KC_NO,    KC_NO,   KC_NO,            KC_NO,                     KC_NO,   KC_NO,    KC_NO,   KC_NO
 	),
 	[2] = LAYOUT_infinity(
